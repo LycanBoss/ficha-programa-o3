b7e7f2e37db637696c23feb5b354b78cc036712a
Adrian Gabriel <lycanboss1995@gmail.com>
2021-09-29 11:11:16 -0300

    Adicionar arquivos de projeto.

---
+++ ficha programação3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ficha_programação3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+            Console.WriteLine("Digite um número positivo ou negativo:");
+            numero = Double.Parse(Console.ReadLine());
+            if (numero >= 0)
+            {
+                numero = Math.Sqrt(numero);
+                Console.WriteLine("A raiz quadrada do número informado é: {0}", numero);
+            }
+            else
+            {
+                numero = Math.Pow(numero, 2);
+                    Console.WriteLine("O valor do número informado elevado a 2 é:{0}", numero);
+            }
+            Console.ReadLine();
+        }
+    }
+}
